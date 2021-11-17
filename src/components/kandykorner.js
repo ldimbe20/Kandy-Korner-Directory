d763c2bcf38c0508f4fade65c55ff71e251f8b2d
ldimbe20 <ldimbe20@gmail.com>
2021-11-17 11:54:07 -0600

    Locations rendering to DOM

---
+++ src/components/kandykorner.js
@@ -0,0 +1,22 @@
+
+import { LocationList } from "./locations/locations"
+
+
+//what state do I want this componant to render
+
+export const KandyKorner = () => { 
+
+    return (
+    <>
+        
+        <h1>Service Tickets</h1>
+        <LocationList />
+
+
+       
+    </>
+
+    )
+} 
+
+// the return of this function is html code or JSX 
---
+++ src/components/locations/locations.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react"
+
+
+
+export const LocationList = () => {
+    const [locations, setLocation] = useState([])
+    
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/location")
+                .then(res => res.json())
+                .then((data) => {
+                    setLocation(data)
+                })
+        },
+        []
+    )
+
+
+
+    return (
+        <>
+            <div>
+                <h2>Locations </h2>
+            </div>
+            {
+                locations.map(
+                    (location) => {
+                        return <p key={`location--${location.id}`}>{location.address}</p>  //the key={`employee--  is used for React as a unique identifier
+                    }
+                )
+            }
+        </>
+    )
+}
